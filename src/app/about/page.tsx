f42765fe0e99af9ce03e1ed4d1ff6e620c207425
Gelbcke <gabrielgelbcke@gmail.com>
2025-02-15 18:02:27 -0300

    creation of base pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AboutPage() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <h1 className="text-4xl font-bold mb-4">About Us</h1>
             <p className="text-lg text-gray-700 mb-4">
                 Welcome to Game Pulse! We are dedicated to bringing you the latest news and updates in the gaming world.
