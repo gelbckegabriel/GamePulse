668e8a858d7a4c3bbc8727f4d80a4b2c551205fe
Gelbcke <gabrielgelbcke@gmail.com>
2025-02-15 18:01:48 -0300

    creation of base environments files

---
+++ src/environments/production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.yourdomain.com',
+    // Ajoutez d'autres configurations spécifiques à l'environnement de production
+  };
