45fe2f269a9a8b16e9a509b0de2ad4ba5a14ba95
Gelbcke <gabrielgelbcke@gmail.com>
2025-02-18 20:52:40 -0300

    checkpoint

--- src/app/shared/footer.tsx
+++ src/app/shared/footer.tsx
@@ -1,8 +1,7 @@
 export default function Footer() {
-    return (
-        <footer className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-4 text-sm text-gray-600">
-            <p>© 2023 Game Pulse</p>
-            <p>Privacy Policy</p>
-        </footer>
-    )
+  return (
+    <></>
+    // https://www.hover.dev/components/other
+    // search for a footer
+  );
 }
--- src/app/shared/header.tsx
+++ src/app/shared/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "./button";
 import { Container } from "./container";
 
@@ -6,16 +7,28 @@ export default function Header() {
     <>
       <header className="bg-backgroundContrast text-white">
         <Container className="flex items-center min-h-[--header-row-height]">
-          <a href="/" className="text-xl -ml-6 flex h-[--header-row-height] items-center px-6">
+          <a
+            href="/"
+            className="text-xl -ml-6 flex h-[--header-row-height] items-center px-6"
+          >
             🍏 <span className="sr-only">Back to homepage</span>
           </a>
+          <div className="flex text-xs gap-5">
+            <Link href="/about">About</Link>
+            <Link href="/leaderboard">Leaderboard</Link>
+            <Link href="/courts">Courts</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/"></Link>
+          </div>
+
+          {/* <Link href="/"></Link> */}
         </Container>
       </header>
       <div className="sticky top-0 z-20 bg-backgroundContrast text-white">
         <Container className="flex items-center justify-between min-h-[--header-row-height]">
-            <p className="text-xl font-semibold">GamePulse</p>
-            <Button size="small">Sign up</Button>
-            </Container>
+          <p className="text-xl font-semibold">GamePulse</p>
+          <Button size="small">Sign up</Button>
+        </Container>
       </div>
     </>
   );
