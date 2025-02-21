7ae9f7a2686055610cc387b680d800042ceb6a3d
Gelbcke <gabrielgelbcke@gmail.com>
2025-02-20 22:23:21 -0300

    create auth modal base

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,56 +1,83 @@
-import Link from "next/link";
-import "./page.scss";
+"use client";
+
+import { useState } from "react";
+import { BsApple, BsGoogle } from "react-icons/bs";
+import { FaFingerprint, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdAlternateEmail } from "react-icons/md";
 
 export default function userAuth() {
-  const cards = [
-    {
-      image: "/auth/user-login.webp",
-      name: "Login",
-      subtitle: "Sign in to your account",
-      bio: "Log in to your GamePulse account to access exclusive features and book a court for your next game!",
-    },
-    {
-      image: "/auth/user-registration.webp",
-      name: "Registration",
-      subtitle: "Create a new account",
-      bio: "Join GamePulse today! Sign up to unlock exclusive features and easily find courts for your next game.",
-    }
-  ];
+  const [showPassword, setShowPassword] = useState<boolean>(true);
+  const togglePasswordView = () => setShowPassword(!showPassword);
 
   return (
     <>
-      <div className="row mt-10" style={{ maxWidth: "100%" }}>
-        {/* CHOOSE DATA-EFFECT BETWEEN: 'zoom', 'blur' and 'color' */}
-        {cards.map((row, index) => (
-          <div className="column" key={index}>
-            <Link
-              href={`/portfolio/${row.name}`}
-              style={{ textDecoration: "none" }}
-            >
-              <div className="card" data-effect="blur">
-                <button className="card__save" type="button">
-                  <i></i>
-                </button>
+      <div className="bg-no-repeat bg-cover bg-center min-h-screen">
+        <div className="w-full h-screen flex items-center justify-center">
+          <div className="w-[90%] max-w-sm md:max-w-md lg:max-w-md p-5 bg-gray-900 flex-col flex items-center gap-3 rounded-xl shadow-slate-500 shadow-lg">
+            <img src="/auth/logo.png" alt="logo" className="w-12 md:w-14" />
+            <h1 className="text-lg md:text-xl font-semibold">Welcome Back</h1>
+            <p className="text-xs md:text-sm text-gray-500 text-center">
+              Don't have an account?{" "}
+              <span className="text-[#0000EE] underline">Sign up</span>
+            </p>
 
-                <figure className="card__image">
-                  <img src={row.image} />
-                </figure>
-
-                <div className="card__header">
-                  <figure className="card__profile">
-                    {/* <img src="./Images/BigData.jpg" alt="Short description" /> */}
-                  </figure>
-                </div>
-
-                <div className="card__body">
-                  <h3 className="card__name">{row.name}</h3>
-                  <p className="card__subtitle">{row.subtitle}</p>
-                  <p className="card__bio">{row.bio}</p>
-                </div>
+            <div className="w-full flex flex-col gap-3">
+              <div className="w-full flex items-center gap-2 bg-gray-800 p-2 rounded-xl">
+                <MdAlternateEmail />
+                <input
+                  type="email"
+                  placeholder="Email address"
+                  className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
+                />
               </div>
-            </Link>
+
+              <div className="w-full flex items-center gap-2 bg-gray-800 p-2 rounded-xl relative">
+                <FaFingerprint />
+                <input
+                  type={showPassword ? "password" : "text"}
+                  placeholder="Password"
+                  className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
+                />
+                {showPassword ? (
+                  <FaRegEyeSlash
+                    className="absolute right-5 cursor-pointer"
+                    onClick={togglePasswordView}
+                  />
+                ) : (
+                  <FaRegEye
+                    className="absolute right-5 cursor-pointer"
+                    onClick={togglePasswordView}
+                  />
+                )}
+              </div>
+            </div>
+
+            <button className="w-full p-2 bg-blue-500 rounded-xl mt-3 hover:bg-blue-600 text-sm md:text-base">
+              Login
+            </button>
+
+            <div className="relative w-full flex items-center justify-center py-3">
+              <div className="w-2/5 h-[2px] bg-gray-800"></div>
+              <h3 className="font-lora text-xs md:text-sm px-4 text-gray-500">
+                Or
+              </h3>
+              <div className="w-2/5 h-[2px] bg-gray-800"></div>
+            </div>
+
+            <div className="w-full flex items-center justify-evenly md:justify-between gap-2">
+              <div className="p-2 md:px-6 lg:px-10 bg-slate-700 cursor-pointer rounded-xl hover:bg-slate-800">
+                <BsApple className="text-lg md:text-xl" />
+              </div>
+              <div className="p-2 md:px-6 lg:px-10 bg-slate-700 cursor-pointer rounded-xl hover:bg-slate-800">
+                <BsGoogle className="text-lg md:text-xl" />
+              </div>
+              <div className="p-2 md:px-6 lg:px-10 bg-slate-700 cursor-pointer rounded-xl hover:bg-slate-800">
+                <FaXTwitter className="text-lg md:text-xl" />
+              </div>
+            </div>
           </div>
-        ))}
+        </div>
       </div>
     </>
   );
