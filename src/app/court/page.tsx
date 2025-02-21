04fa2a0ffa53870428c82aebbaa41b1a934ecada
Gelbcke <gabrielgelbcke@gmail.com>
2025-02-20 21:38:36 -0300

    create standard function for each page

--- src/app/court/page.tsx
+++ src/app/court/page.tsx
@@ -1,2 +1,10 @@
 // COURTS IS GENERAL, EVERYTHING CLOSE TO YOU.
 // COURT IS THE SPECIFIC COURT, YOU CAN SEE MORE DETAILS AND EXECUTE THE REGISTER ITSELF, SEE PLAYER LIST, ETC.
+
+export default function Court() {
+  return (
+    <div>
+      <h1>Court Page</h1>
+    </div>
+  );
+}
--- src/app/courts/page.tsx
+++ src/app/courts/page.tsx
@@ -1,1 +1,9 @@
 // EVERY COURT AVAILABLE, YOU CAN FILTER BY COUNTRY, CITY, AND COURT NAME. AS WELL AS ORDER BY DISTANCE.
+
+export default function Courts() {
+  return (
+    <div>
+      <h1>Court Page</h1>
+    </div>
+  );
+}
