80899fb4dd098763dafeb76e4902835cc6841df6
Gelbcke <gabrielgelbcke@gmail.com>
2025-02-22 16:16:11 -0300

    checkpoint - auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,10 +3,10 @@
 import { useState } from "react";
 import { FaFingerprint, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { FaFacebook, FaGoogle, FaXTwitter } from "react-icons/fa6";
-import { MdAlternateEmail, MdEmail } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
 
 export default function userAuth() {
-  const [showPassword, setShowPassword] = useState<boolean>(true);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const togglePasswordView = () => setShowPassword(!showPassword);
 
   return (
@@ -14,17 +14,17 @@ export default function userAuth() {
       {/* <div className="bg-background h-[100vh]"> */}
       {/* <div className="bg-white h-[100vh]"> */}
       <div className="bg-[url('/home/basketball2.webp')] h-[100vh]">
-        <div className="mx-auto w-[80vh] h-[70vh] p-12 bg-white bg-opacity-40 backdrop-blur-md border border-white border-opacity-50 rounded-lg shadow-lg">
+        <div className="mx-auto w-[80vh] h-[80vh] p-12 bg-white bg-opacity-40 backdrop-blur-md border border-white border-opacity-50 rounded-lg shadow-lg">
           {/* REGISTER WITH */}
           <div className="flex flex-col align-center items-center justify-center">
             <p className="text-sm2">Register with:</p>
             <div className="flex justify-between w-full pt-4">
-              <button className="flex justify-center p-2 rounded-lg bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg min-w-[45%]">
+              <button className="flex justify-center p-2 bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md rounded-lg shadow-lg min-w-[45%]">
                 <FaFacebook className="text-lg md:text-xl" />
                 <span className="pl-2 text-sm">Facebook</span>
               </button>
 
-              <button className="flex justify-center p-2 rounded-lg bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg min-w-[45%]">
+              <button className="flex justify-center p-2 bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md rounded-lg shadow-lg min-w-[45%]">
                 <FaGoogle className="text-lg md:text-xl" />
                 <span className="pl-2 text-sm">Google</span>
               </button>
@@ -40,28 +40,48 @@ export default function userAuth() {
 
           {/* FORM */}
           <div className="login_form">
-            <p className="text-sm2">Username or email</p>
+            <p className="text-sm2 mb-1">Username or email</p>
             <div className="bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg w-full flex items-center gap-2 p-2 rounded-xl">
               <MdEmail />
               <input
                 type="email"
-                className="bg-transparent border-0 w-full outline-none text-sm"
+                style={{ backgroundColor: "transparent" }}
+                className="pl-1 border-0 w-full outline-none text-sm"
               />
             </div>
-            <div className="flex justify-between">
-              <p>Password</p>
-              <p>Forgot Password?</p>
+            <div className="mt-6 flex justify-between">
+              <p className="text-sm2 mb-1">Password</p>
+              <p className="text-sm">Forgot Password?</p>
             </div>
-            <input type="password" placeholder="Password" className="" />
-            <div>
+            <div className="bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg w-full flex items-center gap-2 p-2 rounded-xl">
+              <FaFingerprint />
+              <input
+                type={showPassword ? "text" : "password"}
+                style={{ backgroundColor: "transparent" }}
+                className="pl-1 border-0 w-full outline-none text-sm"
+              />
+              {showPassword ? (
+                <FaRegEye
+                  className="cursor-pointer mr-1"
+                  onClick={togglePasswordView}
+                />
+              ) : (
+                <FaRegEyeSlash
+                  className="cursor-pointer mr-1"
+                  onClick={togglePasswordView}
+                />
+              )}
+            </div>
+
+            {/* <div>
               <input type="checkbox" />
               <p>Remember me</p>
-            </div>
-            <button>Log In</button>
+            </div> */}
+            <button className="mt-8 py-2.5 px-10 w-full bg-white bg-opacity-75 border-white border-opacity-80 backdrop-blur-md rounded-lg shadow-lg text-sm2">Login</button>
           </div>
 
           {/* FOOTER */}
-          <div className="footer">
+          <div className="mt-8 flex justify-center text-sm">
             <p>Do not have an account? Sign Up</p>
           </div>
         </div>
