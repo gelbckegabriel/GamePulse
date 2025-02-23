7db32cb0be70a0de2c7009415ba0f6632c9fd6d7
Gelbcke <gabrielgelbcke@gmail.com>
2025-02-23 15:24:09 -0300

    form complete, needs to refine

--- src/app/shared/create-user.tsx
+++ src/app/shared/create-user.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import {
   useAnimate,
   useDragControls,
   useMotionValue,
   motion,
 } from "framer-motion";
+import {
+  FaFingerprint,
+  FaRegEye,
+  FaRegEyeSlash,
+  FaRegUserCircle,
+} from "react-icons/fa";
+import { FaPerson } from "react-icons/fa6";
+import { IoMdPerson } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
 import useMeasure from "react-use-measure";
 
 type Props = {
@@ -12,6 +24,8 @@ type Props = {
 };
 
 export const CreateUser = ({ openCreate, setOpenCreate }: Props) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const togglePasswordView = () => setShowPassword(!showPassword);
   const [scope, animate] = useAnimate();
   const [drawerRef, { height }] = useMeasure();
 
@@ -83,7 +97,90 @@ export const CreateUser = ({ openCreate, setOpenCreate }: Props) => {
 
             <div className="relative z-0 h-full overflow-y-scroll p-4 pt-12">
               <div className="mx-auto max-w-2xl space-y-4 text-neutral-400 text-white">
-                <h2 className="text-4xl font-bold">Create a new account</h2>
+                <h2 className="text-4xl font-bold mx-auto">
+                  Create a new account
+                </h2>
+
+                <div className="form">
+                  {/* NAME AND LASTNAME CONTAINER */}
+                  <div className="flex justify-between">
+                    <div className="w-[40%] md:w-[35%] lg:w-[35%]">
+                      <p className="text-sm2 mb-1">First Name</p>
+                      <div className="bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg w-full flex items-center gap-2 p-2 rounded-xl">
+                        <IoMdPerson />
+                        <input
+                          type="text"
+                          style={{ backgroundColor: "transparent" }}
+                          className="pl-1 border-0 w-full outline-none text-sm"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="w-[55%]">
+                      <p className="text-sm2 mb-1">Last Name</p>
+                      <div className="bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg w-full flex items-center gap-2 p-2 rounded-xl">
+                        <FaPerson />
+                        <input
+                          type="text"
+                          style={{ backgroundColor: "transparent" }}
+                          className="pl-1 border-0 w-full outline-none text-sm"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* EMAIL FIELD */}
+                  <p className="mt-6 text-sm2 mb-1">Username</p>
+                  <div className="bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg w-full flex items-center gap-2 p-2 rounded-xl">
+                    <FaRegUserCircle />
+                    <input
+                      type="text"
+                      style={{ backgroundColor: "transparent" }}
+                      className="pl-1 border-0 w-full outline-none text-sm"
+                    />
+                  </div>
+
+                  {/* EMAIL FIELD */}
+                  <p className="mt-6 text-sm2 mb-1">Email</p>
+                  <div className="bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg w-full flex items-center gap-2 p-2 rounded-xl">
+                    <MdEmail />
+                    <input
+                      type="email"
+                      style={{ backgroundColor: "transparent" }}
+                      className="pl-1 border-0 w-full outline-none text-sm"
+                    />
+                  </div>
+
+                  {/* PASSWORD FIELD */}
+                  <p className="mt-6 text-sm2 mb-1">Password</p>
+                  <div className="bg-white bg-opacity-15 border-white border-opacity-10 backdrop-blur-md shadow-lg w-full flex items-center gap-2 p-2 rounded-xl">
+                    <FaFingerprint />
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      style={{ backgroundColor: "transparent" }}
+                      className="pl-1 border-0 w-full outline-none text-sm"
+                    />
+                    {showPassword ? (
+                      <FaRegEye
+                        className="cursor-pointer mr-1"
+                        onClick={togglePasswordView}
+                      />
+                    ) : (
+                      <FaRegEyeSlash
+                        className="cursor-pointer mr-1"
+                        onClick={togglePasswordView}
+                      />
+                    )}
+                  </div>
+                </div>
+
+                {/* CREATE BUTTON */}
+                <div className="create">
+                  <button>Create Account</button>
+                </div>
+
+                {/* TERMS OF ACCEPTANCE */}
+                <div className="terms"></div>
               </div>
             </div>
           </motion.div>
