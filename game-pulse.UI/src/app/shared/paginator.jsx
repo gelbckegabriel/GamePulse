e492ec1631fdb51b7d332b79600b7c32e8ce2f84
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-03-25 16:22:50 -0300

    small adjust to paginator - for now

--- game-pulse.UI/src/app/shared/paginator.jsx
+++ game-pulse.UI/src/app/shared/paginator.jsx
@@ -5,15 +5,14 @@ import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
 export default function Paginator({ index }) {
   const [active, setActive] = useState(index);
 
-  const getItemProps = (index) =>
-    ({
-      variant: active === index ? "filled" : "text",
-      color: "gray",
-      onClick: () => setActive(index),
-    });
+  const getItemProps = (index) => ({
+    variant: active === index ? "filled" : "text",
+    color: "gray",
+    onClick: () => setActive(index),
+  });
 
   const next = () => {
-    if (active === 5) return;
+    if (active === 3) return;
 
     setActive(active + 1);
   };
@@ -26,28 +25,16 @@ export default function Paginator({ index }) {
 
   return (
     <>
-      <div className="flex items-center gap-4">
-        <Button
-          variant="text"
-          className="flex items-center gap-2"
-          onClick={prev}
-          disabled={active === 1}
-        >
+      <div className="flex items-center gap-3">
+        <Button variant="text" className="flex items-center gap-2" onClick={prev} disabled={active === 1}>
           <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
         </Button>
         <div className="flex items-center gap-2">
           <IconButton {...getItemProps(1)}>1</IconButton>
           <IconButton {...getItemProps(2)}>2</IconButton>
           <IconButton {...getItemProps(3)}>3</IconButton>
-          <IconButton {...getItemProps(4)}>4</IconButton>
-          <IconButton {...getItemProps(5)}>5</IconButton>
         </div>
-        <Button
-          variant="text"
-          className="flex items-center gap-2"
-          onClick={next}
-          disabled={active === 5}
-        >
+        <Button variant="text" className="flex items-center gap-2" onClick={next} disabled={active === 3}>
           Next
           <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
         </Button>
