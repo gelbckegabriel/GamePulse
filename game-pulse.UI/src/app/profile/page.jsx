93d14777cdec29605a2d967eab1b37c2058cd1f9
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-03-27 20:18:45 -0300

    checkpoint on map location

--- game-pulse.UI/src/app/profile/page.jsx
+++ game-pulse.UI/src/app/profile/page.jsx
@@ -369,9 +369,10 @@ export default function Profile() {
                   <section className="mapbox" data-mapbox>
                     <figure>
                       <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9%3A0x921f6aaa74197fdb!2sSacramento%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd"
-                        width="400"
-                        height="300"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230503.43746694!2d-49.43401161685449!3d-25.48448775126223!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce35351cdb3dd%3A0x6d2f6ba5bacbe809!2sCuritiba%2C%20Paran%C3%A1!5e0!3m2!1sfr!2sbr!4v1743117337947!5m2!1sfr!2sbr"
+                        width="600"
+                        height="450"
+                        allowFullScreen=""
                         loading="lazy"
                       ></iframe>
                     </figure>
