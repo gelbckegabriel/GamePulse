1ef40e387ac273e5ba03b3cc859975d1e41ab394
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-04-03 14:46:58 -0300

    update the board

--- game-pulse.UI/src/app/leaderboard/page.jsx
+++ game-pulse.UI/src/app/leaderboard/page.jsx
@@ -12,27 +12,27 @@ import "./page.scss";
 export default function Leaderboard() {
   // COLORS
   const colors = {
-    mercedes: "#00D2BE",
+    basketball: "#FF8700",
+    football: "#9B0000",
+    volleyball: "#00D2BE",
+    tennis: "#FFF500",
     ferrari: "#DC0000",
-    redBull: "#1E41FF",
-    renault: "#FFF500",
-    racingPoint: "#F596C8",
-    alfaRomeo: "#9B0000",
     toroRosso: "#469BFF",
-    haas: "#BD9E57",
-    mclaren: "#FF8700",
     williams: "#FFFFFF",
   };
 
   // PLAYERS
   const leaderboard = [
-    { name: "Gabriel Gelbcke", team: "mercedes", points: "Leader" },
-    { name: "John Doe", team: "mercedes", points: 1.547 },
-    { name: "Rodolfo Malagueta", team: "ferrari", points: 1.359 },
-    { name: "Peter Jaine", team: "red bull", points: 1.299 },
-    { name: "Pedro Pedrado", team: "ferrari", points: 1.099 },
-    { name: "Paula Torando", team: "red bull", points: 874 },
-    { name: "Mary Doe", team: "renault", points: 474 },
+    { name: "Gabriel Gelbcke", sport: "basketball", awards: 17, points: "Leader" },
+    { name: "John Doe", sport: "football", awards: 13, points: 1.547 },
+    { name: "Rodolfo Malagueta", sport: "volleyball", awards: 15, points: 1.359 },
+    { name: "Peter Jaine", sport: "basketball", awards: 3, points: 1.299 },
+    { name: "Pedro Pedrado da Pedra Rochosa", sport: "volleyball", awards: 9, points: 1.099 },
+    { name: "Paula Tejando Torando", sport: "football", awards: 7, points: 873 },
+    { name: "Mary Doe", sport: "tennis", awards: 4, points: 674 },
+    { name: "Josefina Margherita", sport: "volleyball", awards: 11, points: 529 },
+    { name: "Dolores Analgésica", sport: "football", awards: 9, points: 468 },
+    { name: "Jacinto Lévis", sport: "football", awards: 1, points: 314 },
   ];
 
   return (
@@ -101,13 +101,14 @@ export default function Leaderboard() {
 
       <br />
 
-      <Container>
+      <Container className="">
         {/* Leaderboard */}
         <table className="text-white w-full mx-auto">
           <thead>
             <tr>
               <th>Pos</th>
               <th>Player</th>
+              <th>Best Player Award</th>
               <th>Points</th>
             </tr>
           </thead>
@@ -116,14 +117,27 @@ export default function Leaderboard() {
               <React.Fragment key={index}>
                 <tr className="player">
                   <td className="position">{index + 1}</td>
-                  <td className="player px-4 py-2 text-left border-l-4" style={{ borderColor: colors[player.team] }}>
-                    {player.name.split(" ").map((part, idx) => (
-                      <React.Fragment key={idx}>{idx > 0 ? <strong>{part}</strong> : part} </React.Fragment>
-                    ))}
-                    <span>{player.team}</span>
+                  <td
+                    className="player text-[0.875rem] md:text-[1.1rem] px-4 py-2 text-left border-l-4"
+                    style={{ borderColor: colors[player.sport] }}
+                  >
+                    {(() => {
+                      const nameParts = player.name.split(" ");
+                      const firstName = nameParts[0];
+                      const lastName = nameParts[nameParts.length - 1];
+                      return (
+                        <>
+                          {firstName} <strong>{lastName}</strong>
+                        </>
+                      );
+                    })()}
+                    <span className="text-[0.65rem] md:text-[0.8rem]">{player.sport}</span>
                   </td>
-                  <td className="points">
-                    <span>{player.points}</span>
+                  <td className="score">
+                    <span className="text-[0.65rem] md:text-[0.8rem]">{player.awards}</span>
+                  </td>
+                  <td className="score">
+                    <span className="text-[0.65rem] md:text-[0.8rem]">{player.points}</span>
                   </td>
                 </tr>
                 {index < leaderboard.length - 1 ? <tr className="h-[20px]"></tr> : null}
