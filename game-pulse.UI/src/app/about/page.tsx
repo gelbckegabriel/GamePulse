879eef4d08ec959c29c9e1f98497951ed190915b
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-04-08 19:09:10 -0300

    need to improve about

--- game-pulse.UI/src/app/about/page.tsx
+++ game-pulse.UI/src/app/about/page.tsx
@@ -2,11 +2,19 @@
 
 import React from "react";
 import { Container } from "../shared/container";
+import { Vortex } from "../shared/vortex";
 
 export default function AboutPage() {
   return (
     <>
       <div className="bg-black">
+        <div className="w-[100%] mx-auto rounded-md h-[30rem] overflow-hidden">
+          <Vortex backgroundColor="black" className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+            <h2 className="text-white text-2xl md:text-4xl font-bold text-center">Game Pulse</h2>
+            <p className="text-white text-sm md:text-xl max-w-xl mt-6 text-center">Learn more about the project and the developer.</p>
+          </Vortex>
+        </div>
+
         <Container>
           <span>Vortex Background</span>
           <span>Evervault Card</span>
