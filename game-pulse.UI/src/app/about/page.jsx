fecc4d63b38b8ece62c7057854bad5a6b4dbcc11
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-04-13 15:29:42 -0300

    new style card

--- game-pulse.UI/src/app/about/page.jsx
+++ game-pulse.UI/src/app/about/page.jsx
@@ -4,18 +4,19 @@ import React, { useRef } from "react";
 import { Container } from "../shared/container";
 import { Vortex } from "../shared/vortex";
 import { motion } from "framer-motion";
+import { EvervaultCard, Icon } from "../shared/evervault-card";
 
 export default function AboutPage() {
-  const scrollRef = useRef(null);
+  const ref = useRef(null);
 
   const variantsLeft = {
-    offscreen: { opacity: 0, x: -300, transition: { duration: 0.8 } },
-    onscreen: { opacity: 1, x: 0, transition: { duration: 0.8 } },
+    offscreen: { opacity: 0, y: -300, transition: { duration: 0.8 } },
+    onscreen: { opacity: 1, y: 0, transition: { duration: 0.8 } },
   };
 
   const variantsRight = {
-    offscreen: { opacity: 0, x: 300, transition: { duration: 0.8 } },
-    onscreen: { opacity: 1, x: 0, transition: { duration: 0.8 } },
+    offscreen: { opacity: 0, y: 300, transition: { duration: 0.8 } },
+    onscreen: { opacity: 1, y: 0, transition: { duration: 0.8 } },
   };
 
   return (
@@ -31,10 +32,10 @@ export default function AboutPage() {
         <Container>
           {/* About the Developer */}
           <motion.div
-            ref={scrollRef}
+            ref={ref}
             initial="offscreen"
             whileInView="onscreen"
-            viewport={{ amount: 0.3 }}
+            viewport={{ amount: 0.3, once: true }}
             className="mt-14 md:mt-32 flex flex-row justify-center items-center gap-4"
           >
             <motion.div variants={variantsLeft} className="w-[50%] md:w-[60%] flex flex-col justify-center">
@@ -62,8 +63,23 @@ export default function AboutPage() {
             </p>
           </div>
 
+          <div className="mt-20">
+            <div className="border border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+              <Icon className="absolute h-6 w-6 -top-3 -left-3 text-white" />
+              <Icon className="absolute h-6 w-6 -bottom-3 -left-3 text-white" />
+              <Icon className="absolute h-6 w-6 -top-3 -right-3 text-white" />
+              <Icon className="absolute h-6 w-6 -bottom-3 -right-3 text-white" />
+
+              <EvervaultCard text="hover" />
+
+              <h2 className="text-white mt-4 text-sm font-light">Would you like to see the code behind this website and how everything was built?</h2>
+              <a href="http://github.com" target="_blank" rel="noopener noreferrer">
+                <p className="text-sm border font-light border-white/[0.2] rounded-full mt-4 text-white px-2 py-0.5">View GitHub Repo</p>
+              </a>
+            </div>
+          </div>
+
           <div className="h-[25rem]"></div>
-          <span>Vortex Background</span>
           <span>Evervault Card</span>
           <span>Placeholders and Vanish Input</span>
         </Container>
