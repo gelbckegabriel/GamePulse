e1708a01ebc6d24f75a1b24d96d68362b46e0e3d
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-05-19 20:52:49 -0300

    test commit

--- game-pulse.Data/Models/Court.cs
+++ game-pulse.Data/Models/Court.cs
@@ -11,6 +11,8 @@ public partial class Court
 
     public string City { get; set; } = null!;
 
+    public string State { get; set; } = null!;
+
     public string Country { get; set; } = null!;
 
     public string Address { get; set; } = null!;
