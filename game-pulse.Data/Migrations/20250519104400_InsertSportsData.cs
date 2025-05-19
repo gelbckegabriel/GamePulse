19b1f57c7d1bb0c3524d87b54d34636f348833fb
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-05-19 09:24:48 -0300

    add the sports data

--- game-pulse.Data/Migrations/20250519104400_InsertSportsData.cs
+++ game-pulse.Data/Migrations/20250519104400_InsertSportsData.cs
@@ -15,13 +15,35 @@ namespace game_pulse.Data.Migrations
                 columns: ["id", "name"],
                 values: new object[,]
                 {
-                                { 1, "Basketball" },
-                                { 2, "Football" },
-                                { 3, "Volleyball" },
-                                { 4, "Tennis" }
+                    { 1, "Basketball" },
+                    { 2, "Football" },
+                    { 3, "Volleyball" },
+                    { 4, "Tennis" }
                 });
 
-            // TODO: Add game positions (id, sport_id, name)
+            migrationBuilder.InsertData(
+                table: "sports_positions",
+                columns: ["id", "sport_id", "name"],
+                values: new object[,]
+                {
+                    { 1, 1, "Point Guard" },
+                    { 2, 1, "Shooting Guard" },
+                    { 3, 1, "Small Forward" },
+                    { 4, 1, "Power Forward" },
+                    { 5, 1, "Center" },
+                    { 6, 2, "Goalkeeper" },
+                    { 7, 2, "Defender" },
+                    { 8, 2, "Midfielder" },
+                    { 9, 2, "Attacker" },
+                    { 10, 3, "Outside Hitter" },
+                    { 11, 3, "Opposite Hitter" },
+                    { 12, 3, "Middle Blocker" },
+                    { 13, 3, "Setter" },
+                    { 14, 3, "Libero" },
+                    { 15, 3, "Defensive Specialist" },
+                    { 16, 4, "Singles Tennis" },
+                    { 17, 4, "Doubles Tennis" },
+                });
         }
 
         /// <inheritdoc />
@@ -31,6 +53,12 @@ namespace game_pulse.Data.Migrations
                 "sports",
                 "id",
                 [1, 2, 3, 4]
+                );
+
+            migrationBuilder.DeleteData(
+                "sports_positions",
+                "id",
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
                 );
         }
     }
