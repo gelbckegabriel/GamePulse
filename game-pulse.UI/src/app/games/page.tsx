641f442001951eb6bad1bbf078e79aed4fccc35d
gelbckegabriel <gabrielgelbcke@gmail.com>
2025-05-26 07:56:23 -0300

    small work on the games page

--- game-pulse.UI/src/app/games/page.tsx
+++ game-pulse.UI/src/app/games/page.tsx
@@ -1,14 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { GenericTable } from "../shared/generic-table/generic-table";
+import { Container } from "../shared/utilities/container";
+
 export default function GamesPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [games, setGames] = useState([
+    { court_name: "Parque Atuba", sport: "Basketball", date: "2023-10-01", time: "10:00", players: 10 },
+    { court_name: "Parque Milharel", sport: "Volleyball", date: "2024-17-03", time: "14:00", players: 7 },
+    { court_name: "Parque dos Santos", sport: "Basketball", date: "2025-11-07", time: "09:00", players: 4 },
+    { court_name: "Parque Barigui", sport: "Football", date: "2025-12-18", time: "19:00", players: 9 },
+    { court_name: "Parque de São dos Pinhais", sport: "Basketball", date: "2026-01-04", time: "21:00", players: 1 },
+  ]);
+
   return (
     <>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-4xl font-bold mb-4">Games</h1>
-        <p className="text-lg text-gray-700 mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-        <p className="text-lg text-gray-700">
-          Labore pariatur ipsam debitis! Odit expedita tempora, officiis incidunt tenetur officia eius quas, omnis enim a voluptatibus in nobis
-          molestias at sapiente?
-        </p>
+      <div className="mx-auto text-center text-white my-20">
+        <span className="text-4xl font-bold">WORK IN PROGRESS</span>
       </div>
+      <Container>
+        {/* GAMES TABLE */}
+        <GenericTable tableType="games" columns={["Game", "Court", "Date", "Time", "Players"]} data={games} isLoading={isLoading} />
+      </Container>
     </>
   );
 }
--- game-pulse.UI/src/app/shared/generic-table/generic-table.jsx
+++ game-pulse.UI/src/app/shared/generic-table/generic-table.jsx
@@ -3,6 +3,7 @@ import { LeaderboardBodyTable } from "./table-types/leaderboard-table";
 import { RankBodyTable } from "./table-types/rank-table";
 
 import "./generic-table.scss";
+import { GamesBodyTable } from "./table-types/games-table";
 
 export const GenericTable = ({ tableType, columns, data, isLoading }) => {
   const renderTableBody = () => {
@@ -11,6 +12,8 @@ export const GenericTable = ({ tableType, columns, data, isLoading }) => {
         return <LeaderboardBodyTable data={data} isLoading={isLoading} />;
       case "rank":
         return <RankBodyTable data={data} isLoading={isLoading} />;
+      case "games":
+        return <GamesBodyTable data={data} isLoading={isLoading} />;
     }
   };
 
